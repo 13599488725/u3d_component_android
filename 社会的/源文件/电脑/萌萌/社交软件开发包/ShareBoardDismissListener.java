13a2ecc183791856f05555cbf50d998ba55cdebb
13599488725 <91053672+13599488725@users.noreply.github.com>
2021-09-20 17:11:40 +0800

    Update and rename social/src/com/umeng/socialsdk/ShareBoardDismissListener.java to 社会的/源文件/电脑/萌萌/社交软件开发包/ShareBoardDismissListener.java

---
+++ 社会的/源文件/电脑/萌萌/社交软件开发包/ShareBoardDismissListener.java
@@ -0,0 +1,23 @@
+package com.umeng.socialsdk;
+
+public interface ShareBoardDismissListener {
+	public void onDismiss();
+}
+@Override
+
+publicvoid onCreate(){
+
+super.onCreate();
+
+UMU3DCommonSDK.init(this,"59892f08310c9307b60023d0","Umeng",UMConfigure.DEVICE_TYPE_PHONE,
+
+"669c30a9584623e70e8cd01b0381dcb4");
+
+initUpush();
+
+}[UNUMConfigure initWithAppkey:@"599d6d81c62dca07c5001db6" channel:@"App Store"];
+<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
+<uses-permission android:name="android.permission.INTERNET"/>
+<uses-permission android:name="android.permission.READ_PHONE_STATE"/>
+<uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>
+
